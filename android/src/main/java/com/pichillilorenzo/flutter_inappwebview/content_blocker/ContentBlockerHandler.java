e15dd62bfb4681cbaf16ca76f3bdd7c6dc78105f
clgg <chen3273099@163.com>
2022-12-21 11:37:11 +0800

    Update ContentBlockerHandler.java
    delete 不安全证书验证

--- android/src/main/java/com/pichillilorenzo/flutter_inappwebview/content_blocker/ContentBlockerHandler.java
+++ android/src/main/java/com/pichillilorenzo/flutter_inappwebview/content_blocker/ContentBlockerHandler.java
@@ -181,7 +181,7 @@ public class ContentBlockerHandler {
                             Response response = null;
 
                             try {
-                                response = Util.getUnsafeOkHttpClient().newCall(mRequest).execute();
+                                response = Util.getBasicOkHttpClient().newCall(mRequest).execute();
                                 byte[] dataBytes = response.body().bytes();
                                 InputStream dataStream = new ByteArrayInputStream(dataBytes);
 
@@ -231,7 +231,7 @@ public class ContentBlockerHandler {
             Request mRequest = new Request.Builder().url(url).head().build();
             Response response = null;
             try {
-                response = Util.getUnsafeOkHttpClient().newCall(mRequest).execute();
+                response = Util.getBasicOkHttpClient().newCall(mRequest).execute();
 
                 if (response.header("content-type") != null) {
                     String[] contentTypeSplitted = response.header("content-type").split(";");
